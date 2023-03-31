4886f07579468af15b285b0b8645c22cde315cfa
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-31 00:00:57 -0300

    fix: configuração do main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+
+from fastapi import FastAPI
+
+from api.v1.api import api_router
+from config.settings import settings
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+app = FastAPI(title=settings.PROJECT_NAME,
+              description=settings.PROJECT_DESCRIPTION,
+              version=settings.APP_VERSION)
+
+app.include_router(api_router, prefix=settings.API_V1_PREFIX)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    logging.info("Iniciando servidor...")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
